ba351c840d1db8b60d9683cfeddfc95b41e4645a
knausb <briank.lists@gmail.com>
2021-07-11 11:46:13 -0700

    made first function: R/GAPIT3documentation.R

--- R/GAPIT3documentation.R
+++ R/GAPIT3documentation.R
@@ -1,7 +1,10 @@
 
 
 GAPIT3documentation <- function(){
-  cat("It works!\n")
+  # cat("It works!\n")
+  cat("Welcome to GAPIT3!\n")
+  cat('See:\n\tbrowseVignettes("GAPIT3documentation")\n\nfor documentation.\n')
+  # print('See: browseVignettes("GAPIT3documentation")')
   # browseVignettes("GAPIT3documentation")
   
   return(invisible(NULL))
